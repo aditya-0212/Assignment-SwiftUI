5a80edc6f754901e94264d7b901c424c676c4a5d
aditya choubisa <adityachoubisa02@gmail.com>
2024-11-11 12:59:33 +0530

    Initial Commit

---
+++ Assignment-SwiftUI/Assignment_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_SwiftUIApp.swift
+//  Assignment-SwiftUI
+//
+//  Created by APPLE on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
